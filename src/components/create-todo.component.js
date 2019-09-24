3fc40cb2b9503aab5c9e79e912b5c662e8d278dd
jdbanuel <jamesdb1995@gmail.com>
2019-09-23 21:36:29 -0700

    Creating base files and base layoout

---
+++ src/components/create-todo.component.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class CreateTodo extends Component{
+    render() {
+        return (
+            <div>
+                <p>Welcome to the Create Todo Component!</p>
+            </div>
+        )
+    }
+}
